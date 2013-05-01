f73f3f9615c288c3ab23119c5dc0c767978c41a5
Arik Halperin <arik.halperin@gmail.com>
2013-05-01 22:12:45 +0300

    Changes in EvaAPIs prior to releasing the SDK

--- androidts/src/com/evature/search/MyApplication.java
+++ androidts/src/com/evature/search/MyApplication.java
@@ -3,7 +3,6 @@ package com.evature.search;
 // This class is needed so that application crashes are automatically reported back home.
 // The formKey is a Google Docs key that enables the application to fill in an online Google "Excel" form.
 
-import org.acra.ACRA;
 import org.acra.annotation.ReportsCrashes;
 
 import android.app.Application;
@@ -11,8 +10,7 @@ import android.content.Context;
 import android.content.res.Resources;
 import android.util.Log;
 
-import com.evature.search.R;
-import com.evature.search.R.string;
+import com.evaapis.EvaAPIs;
 import com.evature.search.expedia.EvaDatabase;
 
 @ReportsCrashes(formKey = "dDk0dGxhc1B6Z05vaXh3Q0xxWnhnZlE6MQ")
@@ -24,6 +22,8 @@ public class MyApplication extends Application {
 	static String mExpediaClientId = null;
 	private static final String TAG = "MyApplication";
 	private static Context context; // http://stackoverflow.com/a/5114361/78234
+	private String mEvaSiteCode;
+	private String mEvaApiKey;
 
 	@Override
 	public void onCreate() {
@@ -37,6 +37,11 @@ public class MyApplication extends Application {
 		mExpediaApiKey = resources.getString(R.string.EXPEDIA_API_KEY);
 		mExpediaSecret = resources.getString(R.string.EXPEDIA_SECRET);
 		mExpediaClientId = resources.getString(R.string.EXPEDIA_CLIENT_ID);
+		mEvaSiteCode = resources.getString(R.string.EVA_SITE_CODE);
+		mEvaApiKey = resources.getString(R.string.EVA_API_KEY);
+		
+		EvaAPIs.setKeys(mEvaApiKey,mEvaSiteCode); 
+		
 		super.onCreate();
 		MyApplication.context = getApplicationContext();
 
