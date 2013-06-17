457915986d2a78fc02d67e855c37b631c128ba44
Iftah Haimovitch <iftahh@gmail.com>
2013-06-17 15:23:30 +0300

    Fix bug of repeated use of Expedia search task

--- androidts/src/com/evature/search/controllers/activities/MainActivity.java
+++ androidts/src/com/evature/search/controllers/activities/MainActivity.java
@@ -86,7 +86,7 @@ public class MainActivity extends EvaBaseActivity implements TextToSpeech.OnInit
 	SearchVayantTask mSearchVayantTask;
 	SearchTravelportTask mSearchTravelportTask;
 	SwipeyTabsPagerAdapter mSwipeyAdapter;
-	@Inject HotelListDownloaderTask mSearchExpediaTask;
+	HotelListDownloaderTask mSearchExpediaTask;
 		
 	private boolean mIsNetworkingOk = false;
 
@@ -663,6 +663,7 @@ public class MainActivity extends EvaBaseActivity implements TextToSpeech.OnInit
 		handleSayIt(reply); // Say (using TTS) the eva reply
 		if (reply.isHotelSearch()) {
 			Log.d(TAG, "Running Hotel Search!");
+			mSearchExpediaTask = injector.getInstance(HotelListDownloaderTask.class);
 			mSearchExpediaTask.initialize(this, reply, "$");
 			mSearchExpediaTask.execute();
 		}
