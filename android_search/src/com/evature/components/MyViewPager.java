9f0f58289dacf3c05ce0925817f6e4529d6a836c
Iftah Haimovitch <iftahh@gmail.com>
2013-06-18 15:56:19 +0300

    fix for smooth scrolling viewpager

---
+++ android_search/src/com/evature/components/MyViewPager.java
@@ -0,0 +1,50 @@
+package com.evature.components;
+
+import java.lang.reflect.Field;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.Scroller;
+
+public class MyViewPager extends ViewPager   // see http://stackoverflow.com/a/15549865/519995   bug fix for smooth scrolling
+{
+  public MyViewPager( Context context, AttributeSet attrs) 
+  {
+    super( context, attrs );
+    setMyScroller();
+  }
+  private void setMyScroller() 
+  {
+    try 
+    {
+        Class<?> viewpager = ViewPager.class;
+        Field scroller = viewpager.getDeclaredField("mScroller");
+        scroller.setAccessible(true);
+        scroller.set(this, new MyScroller(getContext()));
+    } catch (Exception e) 
+    {
+        e.printStackTrace();
+    }
+  }
+
+  public class MyScroller extends Scroller 
+  {
+    public MyScroller(Context context) 
+    {
+        super(context, new DecelerateInterpolator());
+    }
+
+    @Override
+    public void startScroll(int startX, int startY, int dx, int dy, int duration) 
+    {
+    	Log.d("viewpager", "startx="+startX+" dx="+dx+"  duration="+duration);
+    	if (duration < 500 && duration % 100 == 0) {
+    		duration = 850;
+    	}
+        super.startScroll(startX, startY, dx, dy, duration /* milli seconds */);
+    }
+  }
+} 
