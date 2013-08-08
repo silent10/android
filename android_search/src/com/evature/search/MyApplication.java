6a391737701fd9e9c534d83e60f4d93c2b0a2809
Iftah Haimovitch <iftahh@gmail.com>
2013-08-08 16:37:10 +0300

    restored crash reporting

--- android_search/src/com/evature/search/MyApplication.java
+++ android_search/src/com/evature/search/MyApplication.java
@@ -34,7 +34,7 @@ public class MyApplication extends Application {
 
 		// if (!BuildConfig.DEBUG) // Not when in debug mode!
 		// The following line triggers the initialization of ACRA
-		//ACRA.init(this);
+		ACRA.init(this);
 		Log.d(TAG, "onCreate");
 
 		Resources resources = getResources();
