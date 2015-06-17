3b32652c24a57b471dc8c2113698f208cf25b1eb
Iftah Haimovitch <iftahh@gmail.com>
2015-06-17 10:52:53 +0300

    removed old SDK

--- EvaAPIs/src/com/evaapis/android/EvaSearchReplyListener.java
+++
@@ -1,9 +0,0 @@
-package com.evaapis.android;
-
-import com.evaapis.crossplatform.EvaApiReply;
-
-public interface EvaSearchReplyListener {
-	void onEvaReply(EvaApiReply reply, Object cookie);
-	void onEvaError(String message, EvaApiReply reply, boolean isServerError, Object cookie);
-	void newSessionStarted(boolean selfTriggered);
-}
--- android_search/src/com/virtual_hotel_agent/search/controllers/events/RatingClickedEvent.java
+++
@@ -1,5 +0,0 @@
-package com.virtual_hotel_agent.search.controllers.events;
-
-public class RatingClickedEvent {
-
-}
